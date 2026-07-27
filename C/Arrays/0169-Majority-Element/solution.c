0515d5b21cc610c8f63e6c54c5006b0e5c9d11df
Satya Blessy Shine Gosula <blessyshinegosula@gmail.com>
2026-07-27 20:58:17 +0530

    Add solution for 169 Majority Element

---
+++ C/Arrays/0169-Majority-Element/solution.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+int compare(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+
+int majorityElement(int* nums, int numsSize)
+{
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    return nums[numsSize / 2];
+}
